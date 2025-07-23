5973260fc9e4aac723c5ffbbf2006def30c0514a
mervinbabu008 <51361364+mervinbabu008@users.noreply.github.com>
2025-07-24 00:36:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 9000; // Important for Beanstalk
+
+app.use(express.json());
+
+app.get('/get', (req, res) => {
+  res.json({ msg: "success" });
+});
+
+app.get('/', (req, res) => {
+  res.json({ msg: "connected" });
+});
+
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+});
+
+// const express = require("express")
+// const app = express()
+// const port = process.env.PORT || 9000; // Important for Beanstalk
+// //const port = 9000
+// app.use(express.json())
+// app.get('/get',(req,res)=>{
+//     res.json({msg:"sucess"})
+// })
+// app.get('/',(req,res)=>{
+//     res.json({msg:"connected"})
+// })
+// // app.listen(port,()=>{
+// //     console.log("welcome port 6000")
+// // })
+// module.exports.handler = serverless(app)
+// // const http = require("http")
+// // const url = http.createServer((req,res)=>{
+// //     console.log("welcome http")
+// //     if(req.url =="/getData"){
+// //         res.json({msg:"sucess",data:[]})
+// //     }
+// //     if(req.url =="/"){
+// //         res.end(JSON.stringify({ message: 'Hello, World!' }));
+// //     }
+// // })
+// // const httpport = 7000
+
+// // url.listen(httpport,()=>{
+// //     console.log("welcome port ",+ httpport)
+// // })
+
